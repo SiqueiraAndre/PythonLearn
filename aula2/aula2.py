52c088f19ee5f716a29a091e1cb69710ac425623
André Siqueira <siqueira.andre@gmail.com>
2021-03-02 06:17:48 -0300

    comando print

---
+++ aula2/aula2.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python
+# coding=utf-8
+# print(123456)
+print('André', 'Siqueira', 'Outra coisa', end='')
+print('André', 'Siqueira', sep='-')
+
+'''
+Gerador CPF: 824.176.070-18
+'''
+print('824','176','070', sep='.', end='')
+print('','18', sep='-')
