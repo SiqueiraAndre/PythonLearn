5cc9b8cad718b2fb192c3e4117d29799b63c7c7c
André Siqueira <siqueira.andre@gmail.com>
2021-03-02 05:40:51 -0300

    comentários de código em Python

---
+++ aula1/aula1.py
@@ -0,0 +1,17 @@
+""""
+# Comentario
+print('Linha 1')  # teste
+print('Linha 2')
+# Uma linha
+# Outra linha
+print('Linha 3')
+"""
+# print('Linha 4') # isso mostra Linha 4 na tela
+# print('Linha 5')
+print('Linha 6')  # comentario aqui
+
+'''
+Um Simples comentario
+'''
+
+print(99123)
