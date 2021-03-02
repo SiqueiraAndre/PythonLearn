625a153c700f2f219cca85ea223a30e027873d70
André Siqueira <siqueira.andre@gmail.com>
2021-03-02 06:55:29 -0300

    Strings (texto) e aspas em Python

---
+++ aula3/aula3.py
@@ -0,0 +1,10 @@
+"""
+str - string
+"""
+
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Essa é meu \"texto\" (str).")
+print('Essa é meu \'texto\' (str).')
+print('Essa é meu \n texto (str).')
+print(r'Essa é meu \n texto (str).')
