3b282cb605bfb0f31cee6c821d8981c5c8460995
André Siqueira <siqueira.andre@gmail.com>
2021-03-02 07:10:28 -0300

    Tipos de dados "primitivos"

---
+++ aula4/aula4.py
@@ -0,0 +1,24 @@
+"""
+Tipos de dados
+str - string - textos "Assim" ou 'Assim'
+int - inteiro - 123456 - 0 - 10 - 20 - 40 - 60 - 1000 - 1500
+float - real / ponto flutuante - 10.50 - 1.5 - 10.93 -49.99 -  0.0
+bool - booleano / lógico - True/False
+"""
+
+print ('Texto', type('Texto'))
+print (10, type(10))
+print (9.99, type(9.99))
+print (10 == 9, type(10 == 9))
+
+# String: nome
+print('André Siqueira', type('André Siqueira'))
+
+# Idade: int
+print(35, type(35))
+
+# Altura: float
+print(1.80, type(1.80))
+
+# É maior de idade 10> 20
+print(35 > 18, type(35 > 18))
