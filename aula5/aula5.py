3d4185ad27eab82a686381d9782642a97feab7ee
André Siqueira <siqueira.andre@gmail.com>
2021-03-03 05:58:30 -0300

    Operadores Aritméticos

---
+++ aula5/aula5.py
@@ -0,0 +1,13 @@
+"""
++ soma, - subtração, * multiplicação, / divisão, // divisão c/ arredondamento, ** potenciação, % resto da divisão, () alterar precedencia nas contas
+"""
+
+print('Multiplicação 10 * 10 =', 10 * 10) # Multiplicação
+print('Soma 10 + 10 =', 10 + 10) # Soma
+print('Subtração 10 - 10 =', 10 - 10) # Subtração
+print('Divisão 10 / 2 =', 10 / 2) # Subtração
+print('Divisão c/ arredondamento 10.5 // 3.5 =', 10.5 // 3.5) # Divisão c/ arredondamento
+print('Potenciação 2 ** 10 =', 2 ** 10) # Potenciação
+print('Resto da Divisão 10 % 3 =', 10 % 3) # Resto da Divisão
+print('Alterar a Precedencia (5+2)*10 =', (5+2)*10 ) # Alterar a Precedencia
+print('Conta coreta 5+2*10 =', 5+2*10 ) # Conta correta
